5032c2aebd52747ec78228381b0e319236f89732
eikecsoares <159174362+eikecsoares@users.noreply.github.com>
2024-08-13 13:49:08 -0300

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE alunos(
+  id int(3) unique ,
+  nome varchar(97) not null, 
+  data de nascimento varchar(11),
+  turma varchar(4)
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
